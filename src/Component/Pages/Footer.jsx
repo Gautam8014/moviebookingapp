68343304bfdce8c8ac61f23b97ced2bc7cdd4aed
Gautam8014 <flt.gautam@gmail.com>
2023-11-02 16:49:27 +0530

    youtube

--- src/Component/Pages/Footer.jsx
+++ src/Component/Pages/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import AddLocationAltIcon from '@mui/icons-material/AddLocationAlt';
-
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 
 export const Footer = () => {
@@ -25,13 +25,29 @@ export const Footer = () => {
         © 2023 Manjhi The Bharat Ratna. All rights reserved.
         </div>
 
-        <div className="footIcon">
+        {/* <div className="footIcon">
             <div><FacebookIcon/></div>
             <div><InstagramIcon/></div>
+            <div><YouTubeIcon/></div>
             <div><AddLocationAltIcon/></div>
             
 
-        </div>
+        </div> */}
+
+<div className="footIcon">
+        <a href="https://www.facebook.com">
+          <FacebookIcon />
+        </a>
+        <a href="https://www.instagram.com">
+          <InstagramIcon />
+        </a>
+        <a href="https://www.youtube.com/watch?v=fFoNuX-Diuo">
+          <YouTubeIcon />
+        </a>
+        <a href="https://www.example.com">
+          <AddLocationAltIcon />
+        </a>
+      </div>
 
         <div className="CreatedBy">
           <h3>Website developed by Kalyan Tech❤️</h3>
